020273af0995ac465d673bf8dc68ebb68996b42a
li <1832376466@qq.com>
2019-02-23 17:13:57 +0800

    demo.sh

---
+++ demo.sh
@@ -0,0 +1,17 @@
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  touch index.html css/style.css js/main.js
+  echo " <!DOCTYPE>
+ <title>Hello</title>
+ <h1>Hi</h1>" > index.html 
+ echo "h1{color: red;}" > css/style.css
+ echo "var string = "Hello World"
+ alert(string)" > js/main.js
+  echo 'success'
+  exit
+fi
