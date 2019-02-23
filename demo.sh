9392aa2802ad2b586a06e70a37481c969b1b3a6d
mbyn123 <47594749+mbyn123@users.noreply.github.com>
2019-02-23 17:19:19 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -7,11 +7,11 @@ else
   mkdir css js
   touch index.html css/style.css js/main.js
   echo " <!DOCTYPE>
- <title>Hello</title>
- <h1>Hi</h1>" > index.html 
- echo "h1{color: red;}" > css/style.css
- echo "var string = "Hello World"
- alert(string)" > js/main.js
+         <title>Hello</title>
+         <h1>Hi</h1>" > index.html 
+  echo "h1{color: red;}" > css/style.css
+  echo "var string = "Hello World"
+         alert(string)" > js/main.js
   echo 'success'
   exit
 fi
